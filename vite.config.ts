af21b4a78f217c926fdd1ceea87ee637675a190f
Sergey Ufocoder <xufocoder@gmail.com>
2023-08-28 14:00:41 +0300

    feat: add eslint support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(), 
+    eslintPlugin()
+  ],
   server: {
     port: 3000,
   },
