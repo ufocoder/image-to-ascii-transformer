7d15c25984d940b4e4c66f0e7f6b307c207a6e92
Sergey Ufocoder <xufocoder@gmail.com>
2023-08-28 14:32:26 +0300

    fix: vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import eslintPlugin from 'vite-plugin-eslint';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/image-to-ascii-transformer/',
   plugins: [
     solidPlugin(), 
     eslintPlugin()
