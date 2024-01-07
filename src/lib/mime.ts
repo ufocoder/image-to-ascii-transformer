5ab8015c70bc9c970e80cefdd7d68d224dae84c8
Sergey Ufocoder <xufocoder@gmail.com>
2024-01-07 17:49:21 +0300

    feat: add `getMimeType` func

---
+++ src/lib/mime.ts
@@ -0,0 +1,31 @@
+const extractMimeType = (header: string) => {
+    switch (header) {
+        case "89504e47":
+            return "image/png";
+        case "47494638":
+            return "image/gif";
+        case "ffd8ffe0":
+        case "ffd8ffe1":
+        case "ffd8ffe2":
+        case "ffd8ffe3":
+        case "ffd8ffe8":
+            return "image/jpeg";
+        default:
+            return "unknown";
+    }
+}
+
+export const getMimeType = async (blob: Blob) => new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    
+    fileReader.onloadend = (e) => {
+        const bytes = (new Uint8Array((e.target as any).result)).subarray(0, 4);
+        const header = bytes.reduce((accumulator, byte) => accumulator += byte.toString(16), '');
+
+        resolve(extractMimeType(header));
+    }
+
+    fileReader.onerror = reject;
+    fileReader.onabort = reject;
+    fileReader.readAsArrayBuffer(blob);
+});
