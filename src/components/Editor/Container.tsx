9ebfd25741ea634c9aacfc78135037ae8447dd64
Sergey Ufocoder <xufocoder@gmail.com>
2024-01-16 16:45:28 +0300

    fix: layout

--- src/components/Editor/Container.tsx
+++ src/components/Editor/Container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 export default function Container(props: ContainerProps) {
   return (
-    <div class="flex flex-col items-center justify-center h-full border-black border-dashed rounded-lg bg-gray-50 p-8 mb-2 overflow-hidden">
+    <div class="flex flex-col min-h-80 items-center justify-center h-full border-black border-dashed rounded-lg bg-gray-50 p-8 mb-2 overflow-hidden">
         {props.children}
     </div>
   );
